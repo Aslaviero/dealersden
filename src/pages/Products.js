931489ca279ad0fa753a6302785b8cc972de03c1
Anthony Slaviero <tonyslaviero@gmail.com>
2023-05-09 12:10:16 -0700

    Created components folder and pages folder. Added JS page files Home, Checkout, Products, Cart, Login, Register, SavedItems. Added in a file for Firebase as well as installed npm files for firebase that way users can create accounts.

---
+++ src/pages/Products.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+// Import product data and Product component here
+
+function Products() {
+  // Fetch and display product data here
+  return (
+    <div>
+      <h1>Products</h1>
+      {/* Render  Product components here */}
+    </div>
+  );
+}
+
+export default Products;
